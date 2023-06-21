61e1343d3fc5704d12549c6133781d4b4b5e84d9
Corbinator333 <corbinator@live.com>
2023-06-21 01:02:02 -0700

    Initial Commit

---
+++ src/main/java/turtleboi/experiments/Experiments.java
@@ -0,0 +1,17 @@
+package turtleboi.experiments;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Experiments extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
