8ac6a676cb4a17ec9d63bf56c23b63a442846b52
Corbinator333 <corbinator@live.com>
2023-06-21 14:38:57 -0700

    Automatic version updater

--- src/main/java/turtleboi/experiments/Experiments.java
+++ src/main/java/turtleboi/experiments/Experiments.java
@@ -1,17 +1,35 @@
 package turtleboi.experiments;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.IOException;
+import java.util.Properties;
+
 public final class Experiments extends JavaPlugin {
+    private String version;
+
+    public String propertiesGrab() {
+        final Properties properties = new Properties();
+        try {
+            properties.load(this.getClass().getClassLoader().getResourceAsStream("project.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return properties.getProperty("version");
+    }
 
     @Override
     public void onEnable() {
+        version = propertiesGrab();
         // Plugin startup logic
-
+        Bukkit.getLogger().info("TurtleBoi Experiments working as expected. Version: " + version);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        Bukkit.getLogger().info("TurtleBoi Experiments shutting down. Version: " + version);
     }
 }
+
